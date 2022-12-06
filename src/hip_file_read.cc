c94d78d7a396a11f5b4cd563ca361f86b6351cbf
Edgar Gabriel <edgar.gabriel@amd.com>
2022-12-06 15:29:44 -0600

    add file_read and file_iread tests
    not being added to run_all.sh scripts for now on purpose

--- src/hip_file_read.cc
+++ src/hip_file_read.cc
@@ -20,7 +20,8 @@ int elements=64*1024*1024;
 hip_mpitest_buffer *sendbuf=NULL;
 hip_mpitest_buffer *recvbuf=NULL;
 
-static void SL_read ( int hdl, void *buf, size_t num);
+static void SL_write (int hdl, void *buf, size_t num);
+
 
 static void init_sendbuf (long *sendbuf, int count, int unused)
 {
@@ -52,12 +53,12 @@ static bool check_recvbuf(long *recvbuf, int nprocs_unused, int rank_unused, int
     return res;
 }
 
-int file_write_test (void *sendbuf, int count,
-                     MPI_Datatype datatype, MPI_File fh);
+int file_read_test (void *sendbuf, int count,
+                    MPI_Datatype datatype, MPI_File fh);
 
 int main (int argc, char *argv[])
 {
-    int res;
+    int res, fd, old_mask, perm;
     int rank, size;
     MPI_File fh;
 
@@ -84,15 +85,36 @@ int main (int argc, char *argv[])
     ALLOCATE_RECVBUFFER(recvbuf, tmp_recvbuf, long, elements, sizeof(long),
                         rank, MPI_COMM_WORLD, init_recvbuf);
 
-    // execute file_write test
-    MPI_File_open(MPI_COMM_SELF, "testout.out", MPI_MODE_CREATE|MPI_MODE_WRONLY,
+    // Create input file
+    old_mask = umask(022);
+    umask (old_mask);
+    perm = old_mask^0666;
+
+    fd = open ("testout.out", O_CREAT|O_WRONLY, perm );
+    if (-1 == fd) {
+        fprintf(stderr, "Error in creating input file. Aborting\n");
+        MPI_Abort (MPI_COMM_WORLD, 1);
+        return 1;
+    }
+
+    if (sendbuf->NeedsStagingBuffer()) {
+        HIP_CHECK(sendbuf->CopyFrom(tmp_sendbuf, elements*sizeof(long)));
+        SL_write(fd, tmp_sendbuf, elements*sizeof(long));
+    }
+    else {
+        SL_write(fd, sendbuf->get_buffer(), elements*sizeof(long));
+    }
+    close (fd);
+
+    // execute file_read test
+    MPI_File_open(MPI_COMM_SELF, "testout.out", MPI_MODE_RDONLY,
                   MPI_INFO_NULL, &fh);
     MPI_Barrier(MPI_COMM_WORLD);
     auto t1s = std::chrono::high_resolution_clock::now();
-    res = file_write_test (sendbuf->get_buffer(), elements,
-                           MPI_LONG, fh);
+    res = file_read_test (recvbuf->get_buffer(), elements,
+                          MPI_LONG, fh);
     if (MPI_SUCCESS != res) {
-        fprintf(stderr, "Error in file_write_test. Aborting\n");
+        fprintf(stderr, "Error in file_read_test. Aborting\n");
         MPI_Abort (MPI_COMM_WORLD, 1);
         return 1;
     }
@@ -101,16 +123,18 @@ int main (int argc, char *argv[])
     double t1 = std::chrono::duration<double>(t1e-t1s).count();
 
     // verify results
-    bool ret = true;
-    int fd = open ("testout.out", O_RDONLY );
-    if ( -1 != fd ) {
-        SL_read(fd, tmp_recvbuf, elements * sizeof(long));
+    bool ret;
+    if (recvbuf->NeedsStagingBuffer()) {
+        HIP_CHECK(recvbuf->CopyFrom(tmp_recvbuf, elements*sizeof(long)));
         ret = check_recvbuf(tmp_recvbuf, size, rank, elements);
     }
+    else {
+        ret = check_recvbuf((long*) recvbuf->get_buffer(), size, rank, elements);
+    }
 
-    bool fret = report_testresult(argv[0], MPI_COMM_WORLD, sendbuf->get_memchar(),
-                                  '-', ret);
-    report_performance (argv[0], MPI_COMM_WORLD, sendbuf->get_memchar(), '-',
+    bool fret = report_testresult(argv[0], MPI_COMM_WORLD, '-', recvbuf->get_memchar(),
+                                  ret);
+    report_performance (argv[0], MPI_COMM_WORLD, '-', recvbuf->get_memchar(),
                         elements, (size_t)(elements * sizeof(long)), 1, t1);
 
     //Free buffers
@@ -125,50 +149,45 @@ int main (int argc, char *argv[])
     return fret ? 0 : 1;
 }
 
-int file_write_test (void *sendbuf, int count, MPI_Datatype datatype, MPI_File fh )
+int file_read_test (void *recvbuf, int count, MPI_Datatype datatype, MPI_File fh )
 {
     int ret;
 
-#ifdef HIP_MPITEST_FILE_IWRITE
+#ifdef HIP_MPITEST_FILE_IREAD
     MPI_Request req;
-    ret = MPI_File_iwrite(fh, sendbuf, count, datatype, &req);
+    ret = MPI_File_iread(fh, recvbuf, count, datatype, &req);
     MPI_Wait (&req, MPI_STATUS_IGNORE);
 #else
-    ret = MPI_File_write (fh, sendbuf, count, datatype, MPI_STATUS_IGNORE);
+    ret = MPI_File_read (fh, recvbuf, count, datatype, MPI_STATUS_IGNORE);
 #endif
     return ret;
 }
 
-void SL_read ( int hdl, void *buf, size_t num )
+void SL_write (int hdl, void *buf, size_t num )
 {
     int lcount=0;
     int a;
-    char *wbuf = (char *)buf;
-    do {
-        a = read (hdl, wbuf, num);
+    char *wbuf = ( char *)buf;
 
-        if(0 == a && num > 0){
-            printf("\nSL_read: Warning: # Bytes read are less than "
-                   "expected file size %d %s\n", hdl, strerror(errno));
+    do {
+    a = write ( hdl, wbuf, num);
+    if ( a == -1 ) {
+        if ( errno == EINTR || errno == EAGAIN ||
+             errno == EINPROGRESS || errno == EWOULDBLOCK) {
+            continue;
+        } else {
+            printf("SL_write: error while writing to file %d %s\n", hdl, strerror(errno));
             return;
         }
+        lcount++;
+        a=0;
+    }
 
-        if ( a == -1 ) {
-            if (errno == EINTR       || errno == EAGAIN     ||
-                errno == EINPROGRESS || errno == EWOULDBLOCK) {
-                continue;
-            } else {
-                printf("SL_read: error while reading from file %d %s\n", hdl, strerror(errno));
-                return;
-            }
-            lcount++;
-            a=0;
-        }
+    num -= a;
+    wbuf += a;
 
-        num -= a;
-        wbuf += a;
-
-    } while ( num > 0 &&  lcount < 20 );
+    } while ( num > 0 );
 
     return;
 }
+
